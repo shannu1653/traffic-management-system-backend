89b048dbc182f33c68f5f1dca05b7a63f3f955bf
shannu1653 <pentashanmukha2002@gmail.com>
2026-01-22 07:36:42 +0530

    Fix RegisterView permissions for production

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -3,11 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status,permissions
 from .serializers import RegisterSerializer
 
 class RegisterView(APIView):
-    def post(self, request):
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request, *args, **kwargs):
         serializer = RegisterSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
